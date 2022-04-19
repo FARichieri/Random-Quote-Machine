66f6ddf5bf69602e4b1a1a84361ed0efb78ee6d1
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-19 16:19:01 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div id="quote-box">
+
+    <div id="text">
+      I'm the quote
     </div>
+
+    <div id="author">
+      I'm the actor
+    </div>
+
+    <button id="new-quote">
+      Button
+    </button>
+
+    <a id="tweet-quote">
+      Tweet
+    </a>
+    
+  </div>
   );
 }
 
